0bed22fd6955924bc793b15efc7218e768f7813f
Giuseppe Di Palma <g.dipalma26@studenti.unisa.it>
2021-02-05 15:25:03 +0100

    add question in class 👍

--- src/main/java/exam/gdp/p2p/Question.java
+++ src/main/java/exam/gdp/p2p/Question.java
@@ -1,13 +1,27 @@
 package exam.gdp.p2p;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Question {
 
-    String promt;
-    String answer;
+    List<String> questions = new ArrayList<String>();
 
-    public Question(String promt, String answer) {
-        this.promt = promt;
-        this.answer = answer;
+    public Question() {
+        questions.add("I see myself as someone who...\n" + "\tHa pochi interessi artistici\n");
+        questions.add("I see myself as someone who...\n" + "\tTende ad essere pigro\n");
+        questions.add("I see myself as someone who...\n" + "\tÈ estroverso e socievole\n");
+        questions.add("I see myself as someone who...\n" + "\tHa una fervida immaginazione\n");
+        questions.add("I see myself as someone who...\n" + "\tTende a trovare difetti negli altri\n");
+        questions.add("I see myself as someone who...\n" + "\tSi innervosisce facilmente\n");
+        questions.add("I see myself as someone who...\n" + "\tÈ rilassato e gestisce bene lo stress\n");
+        questions.add("I see myself as someone who...\n" + "\tÈ solitamente fiducioso\n");
+        questions.add("I see myself as someone who...\n" + "\tÈ riservato\n");
+        questions.add("I see myself as someone who...\n" + "\tHa pochi interessi artistici\n");
+    }
+
+    public List<String> returnQuestion() {
+        return questions;
     }
 
 }
