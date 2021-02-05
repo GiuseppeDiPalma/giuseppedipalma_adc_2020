60897e0e8c921e0fcf6d2be7cfa32c91733a702b
Giuseppe Di Palma <g.dipalma26@studenti.unisa.it>
2021-02-05 16:05:32 +0100

    messageListener interface extension 😅

---
+++ src/main/java/exam/gdp/p2p/MessageListenerImpl.java
@@ -0,0 +1,25 @@
+package exam.gdp.p2p;
+
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.beryx.textio.TextTerminal;
+
+public class MessageListenerImpl implements MessageListener {
+
+    int peerid;
+
+    public MessageListenerImpl(int peerid) {
+        this.peerid = peerid;
+
+    }
+
+    public Object parseMessage(Object obj) {
+
+        TextIO textIO = TextIoFactory.getTextIO();
+        TextTerminal terminal = textIO.getTextTerminal();
+        String message = (String) obj;
+        // Simple check with message
+        terminal.printf("\n" + peerid + "] (Direct Message Received) " + message + "\n");
+        return "success";
+    }
+}
