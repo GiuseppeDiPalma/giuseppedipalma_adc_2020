89064ecf05c3fa88ed1dc3c1339bf77e6854c845
Giuseppe Di Palma <g.dipalma26@studenti.unisa.it>
2021-02-05 15:26:34 +0100

    lay hammingDistance function for after 🆕

--- src/main/java/exam/gdp/p2p/Utils.java
+++ src/main/java/exam/gdp/p2p/Utils.java
@@ -1,6 +1,17 @@
 package exam.gdp.p2p;
 
 public class Utils {
-    
+
     final static int MASTER_PORT_BASE = 6000;
+
+    // function to calculate Hamming distance
+    static int hammingDist(String str1, String str2) {
+        int i = 0, count = 0;
+        while (i < str1.length()) {
+            if (str1.charAt(i) != str2.charAt(i))
+                count++;
+            i++;
+        }
+        return count;
+    }
 }
